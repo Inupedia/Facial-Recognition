bedab12069e7d959378f082466214213a122848c
Inupedia <xenthorpis2016@gmail.com>
2022-06-12 00:46:16 -0600

    final version

--- face_train.py
+++ face_train.py
@@ -1,51 +1,102 @@
-import cv2
-import os
+import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
-import pickle
+import os
+from PIL import Image, ImageOps
+import cv2
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 
+# get the current path and located the image directory
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-image_dir = os.path.join(BASE_DIR, "image")
+img_dir = os.path.join(BASE_DIR, "image")
 
+# Cascade Classifier to determine the image face
 face_cascade = cv2.CascadeClassifier('./data/haarcascade_frontalface_alt2.xml')
-recognizer = cv2.face.LBPHFaceRecognizer_create()
 
-
-def LoadData():
+# read the image file and convert it to data (base on grayscale)
+def decompose_image_to_data():
     current_id = 0
-    label_ids = {}
-    y_labels = []
-    x_train = []
-    for root, dirs, files in os.walk(image_dir):
+    label_ids = {} # associate the label with its id, for example {'pengju1': 1, 'pengju2': 2}
+    data = []
+    labels = []
+    IMAGE_SIZE = 300 # smaller size has fewer components
+    # check if there is image exists (through its extension jpg or png in this case)
+    for root, dirs, files in os.walk(img_dir):
         for file in files:
             if file.endswith("png") or file.endswith("jpg"):
+
                 path = os.path.join(root, file)
                 label = os.path.basename(root).replace(" ", "-").lower()
 
+                # note every label in our dataset must be unique
                 if label not in label_ids:
                     label_ids[label] = current_id
                     current_id += 1
-                id_ = label_ids[label]
 
-                pil_image = Image.open(path).convert("L")  # grayscale
-                size = (550, 550)
-                final_image = pil_image.resize(size, Image.ANTIALIAS)
-                image_array = np.array(final_image, "uint8")
+                current_image_id = label_ids[label]
 
-                faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+                # convert image and add its face data to list
+                pil_image = Image.open(path).convert("L")
+                resized_image = pil_image.resize((IMAGE_SIZE, IMAGE_SIZE), Image.ANTIALIAS)
+                image_array = np.array(resized_image, "uint8")
+                # # by using haarcascade_frontalface_alt2 we can find the front face through picture
+                # # note that we only need to save the recognizable image
+                faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.05, minNeighbors=5)
+                for x, y, w, h in faces:
+                    roi = image_array[y: y + h, x: x + w]
+                    resized_array = cv2.resize(roi, dsize=(150, 150), interpolation=cv2.INTER_CUBIC)
+                    reshaped_img = np.reshape(resized_array, (1, 150 * 150))
+                    data.extend(reshaped_img)
+                    #np.concatenate(data, roi)
+                    labels.append(current_image_id)
+    return np.array(data), np.array(labels), label_ids
 
-                for (x, y, w, h) in faces:
-                    roi = image_array[y:y + h, x:x + w]
-                    x_train.append(roi)
-                    y_labels.append(id_)
+def pca_analysis(data):
+    variance_to_keep = 0.9 # in this case we want to keep 90% characteristics
+    my_pca = PCA(n_components = variance_to_keep)
+    transform_data = my_pca.fit(data)
+    return transform_data
 
+def svm_classification(model, label, test_data):
+    clf3 = SVC(C=3.0)
+    clf3.fit(model, label)
+    return clf3.predict(test_data)
 
-    with open("face-labels.pickle", 'wb') as f:
-        pickle.dump(label_ids, f)
+def knn_classification(neighbor, model, label, test_data):
+    knn = KNeighborsClassifier(n_neighbors=neighbor)
+    knn.fit(model, label)
+    return knn.predict(test_data)
 
-    recognizer.train(x_train, np.array(y_labels))
-    recognizer.save("face-trainner.yml")
+def get_name(model, label, label_ids, classification_method, test_data):
 
+    labels = {v: k for k, v in label_ids.items()}
+
+    result = ""
+    if classification_method == "svm":
+        result = svm_classification(model, label, test_data)
+    elif classification_method == "knn":
+        result = knn_classification(5, model, label, test_data) # our k-nearst neighbors is 5 in this case
+
+    return labels[result[0]]
+
+def create_model():
+    data, labels, label_ids = decompose_image_to_data()
+    # scale the dataset
+    scaler = MinMaxScaler()
+    data_scaled = scaler.fit_transform(data)
+    # model = pca_analysis(data_scaled)
+    return data_scaled, labels, label_ids
 
 if __name__ == '__main__':
-    LoadData()
+    data, labels, label_ids = decompose_image_to_data()
+    # scale the dataset
+    scaler = MinMaxScaler()
+    data_scaled = scaler.fit_transform(data)
+
+    transform_data = pca_analysis(data_scaled)
+    #
+    print(transform_data.explained_variance_ratio_)
+
+
--- main.py
+++ main.py
@@ -2,73 +2,59 @@ import face_train
 import face_capture
 import numpy as np
 import cv2
-import pickle
 from sklearn.decomposition import PCA
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import MinMaxScaler
 
+def run(data, labels, label_ids, classification_method):
 
-def svm(trainDataSimplified, trainLabel, testDataSimplified):
-    clf3 = SVC(C=3.0)  
-    clf3.fit(trainDataSimplified, trainLabel)
-    return clf3.predict(testDataSimplified)
+    face_cascade = cv2.CascadeClassifier('./data/haarcascade_frontalface_alt2.xml')
+    cap = cv2.VideoCapture(0)
 
-def knn(neighbor, traindata, trainlabel, testdata):
-    neigh = KNeighborsClassifier(n_neighbors=neighbor)
-    neigh.fit(traindata, trainlabel)
-    return neigh.predict(testdata)
+    # perform PCA analysis
+    variance_to_keep = 0.9  # in this case we want to keep 90% characteristics
+    my_pca = PCA(n_components=variance_to_keep)
+    model = my_pca.fit_transform(data)
 
-def run():
+    while (True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        image_array = np.array(gray)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
+        for (x, y, w, h) in faces:
 
-	face_cascade = cv2.CascadeClassifier('./data/haarcascade_frontalface_alt2.xml')
-	recognizer = cv2.face.LBPHFaceRecognizer_create()
-	recognizer.read("./face-trainner.yml")
+            roi_gray = image_array[y:y + h, x:x + w]
+            resized_array = cv2.resize(roi_gray, dsize=(150, 150), interpolation=cv2.INTER_CUBIC)
+            reshaped_image = np.reshape(resized_array, (1, 150 * 150))
+            # scale the dataset
+            scaler = MinMaxScaler()
+            data_scaled = scaler.fit_transform(reshaped_image)
+            data_test_transform = my_pca.transform(data_scaled)
 
-	labels = {"person_name": 1}
-	with open("face-labels.pickle", 'rb') as f:
-		og_labels = pickle.load(f)
-		labels = {v:k for k,v in og_labels.items()}
+            name = face_train.get_name(model, labels, label_ids, classification_method, data_test_transform)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            color = (255, 255, 255)
+            stroke = 2
+            cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
 
+            color = (255, 0, 0)  # BGR 0-255
+            stroke = 2
+            end_cord_x = x + w
+            end_cord_y = y + h
+            cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
 
-	cap = cv2.VideoCapture(0)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
 
-	# pca = PCA(0.9, True, True)
+    cap.release()
+    cv2.destroyAllWindows()
 
-	while(True):
-	    # Capture frame-by-frame
-	    ret, frame = cap.read()
-	    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-	    for (x, y, w, h) in faces:
-	    	#print(x,y,w,h)
-	    	roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
-	    	roi_color = frame[y:y+h, x:x+w]
-
-	    	id_, conf = recognizer.predict(roi_gray)
-	    	if conf>=4 and conf <= 85:
-	    		font = cv2.FONT_HERSHEY_SIMPLEX
-	    		name = labels[id_]
-	    		color = (255, 255, 255)
-	    		stroke = 2
-	    		cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
-
-	    	color = (255, 0, 0) #BGR 0-255 
-	    	stroke = 2
-	    	end_cord_x = x + w
-	    	end_cord_y = y + h
-	    	cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
-	    	
-	    cv2.imshow('frame',frame)
-	    if cv2.waitKey(20) & 0xFF == ord('q'):
-	        break
-
-	cap.release()
-	cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-	face_capture.start()
-	print("Processing data...")
-	face_train.LoadData()
-	print("Starting to detect your face from local image source, use CTRL + C to quit.")
-	run()
-    
+    face_capture.start()
+    print("Processing data...")
+    data, labels, label_ids = face_train.create_model()
+    classification_method = input("Which classification method you would like to perform? (svm/knn): ")
+    print("Starting to detect your face from local image source, use CTRL + C to quit.")
+    run(data, labels, label_ids, classification_method)
